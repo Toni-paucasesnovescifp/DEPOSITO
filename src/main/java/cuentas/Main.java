f1d0be3504a569a7ce59ea9f2ec7ab6323f69fdc
Toni Covas <antoniocovas@paucasesnovescifp.cat>
2024-02-10 19:13:45 +0100

    Merge remote-tracking branch 'origin/master'

--- src/main/java/cuentas/Main.java
+++ src/main/java/cuentas/Main.java
@@ -18,7 +18,7 @@ public class Main {
         try {
             miCuenta.retirar(cantidad);
         } catch (Exception e) {
-            System.out.print("Fallo al retirar. Hola compi112");
+            System.out.print("Fallo al retirar. Hola compi142");
         }
         try {
             System.out.println("Ingreso en cuenta");
