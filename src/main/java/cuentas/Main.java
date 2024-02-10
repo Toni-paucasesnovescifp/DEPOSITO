1bb07406b92429293e12373b512192b927764ee4
Toni Covas <antoniocovas@paucasesnovescifp.cat>
2024-02-10 18:32:43 +0100

    actualización proyecto

--- src/main/java/cuentas/Main.java
+++ src/main/java/cuentas/Main.java
@@ -18,7 +18,7 @@ public class Main {
         try {
             miCuenta.retirar(cantidad);
         } catch (Exception e) {
-            System.out.print("Fallo al retirar. Hola compi22");
+            System.out.print("Fallo al retirar. Hola compi42");
         }
         try {
             System.out.println("Ingreso en cuenta");
